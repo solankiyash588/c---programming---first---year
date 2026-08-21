92a2bbb01b18c736591e18929dbb82410eed9bf7
solankiyash588 <parbatahir397@gmail.com>
2026-08-21 09:16:35 +0530

    Add files via upload

---
+++ maths.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two numbers :");
+    scanf("%d %d",&a,&b);
+
+    printf("Subtraction = %d\n ", a - b);
+    printf("Multiplication : %d\n", a * b);
+    printf("Division = %d\n", a/b);
+    return 0;
+}
