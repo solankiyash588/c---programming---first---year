1e094f57774612922b34ecaf806a882b2916807e
solankiyash588 <parbatahir397@gmail.com>
2026-08-27 16:10:33 +0530

    Add files via upload

---
+++ op.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter a number :");
+    scanf("%d",&number);
+
+    if(number >= 0)
+   { printf("Positive\n");
+    if(number % 2 == 0){
+        printf("even\n");}
+        else{
+        printf("odd\n");}
+   }
+ else {
+    printf("negative\n");}
+    return 0;
+}
