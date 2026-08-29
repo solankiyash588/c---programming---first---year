786e8ba36da87b90c9774fef470a045e3bdc3934
solankiyash588 <parbatahir397@gmail.com>
2026-08-29 17:41:06 +0530

    Add files via upload

---
+++ whileloup2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf(" Enter the vale of n :");
+    scanf("%d",&n);
+    int i = 0;
+    while(i <= n)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+return 0;
+}
