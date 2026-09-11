00220bf22d451a71ba7b42daba39e1c5c4b0c8ef
solankiyash588 <parbatahir397@gmail.com>
2026-09-11 21:50:45 +0530

    Add files via upload

---
+++ Area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float a,b,h;
+printf(" Enter height:");
+scanf("%f",&h);
+printf(" Enter base:");
+scanf("%f",&b);
+a = h*b*0.5;
+printf(" Area of Traiangle :%2f\n",a);
+return 0;
+    }
